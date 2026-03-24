65bbfdfbece62e5dcedbec39659b461a187237ce
Dibya-kcd <dibyaprakashkcd@gmail.com>
2026-03-24 04:42:08 +0000

    Deploying to gh-pages from @ Dibya-kcd/mrwaterprov1@ac9fbc3cf6e779ce9326161dcc141978ee6325d2 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "569236607"
+    serviceWorkerVersion: "601808600"
   }
 });
